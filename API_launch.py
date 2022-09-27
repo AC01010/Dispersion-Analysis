5be4a14f5fbabf934cabfd2e0520d3b3fce275f9
Allen Chang <allen.chang@gatech.edu>
2022-09-27 19:54:23 -0400

    Update API_launch.py
    Rewrote API_launch_site, added inv_meter_per_pixel

--- API_launch.py
+++ API_launch.py
@@ -1,21 +1,25 @@
 import math
+import requests
 def API_launch_site(latitude,longitude, zoom_level,length_level,height_level,scale_level,format_type,maptype,key):
-    # ALL INPUTS ARE STRINGS
-    # Longitude and Latitude must be string inputs of 6 decimal digits precission, zoom level ranges from 1 (world zoom) to 20 (buildings zoom)
-    API="https://maps.googleapis.com/maps/api/staticmap?"
-    # We now add the location parameters to the URL: center ({latitude, longitude} coordinates) and zoom (we specify zoom to streets level, with value 15):
-    API=API+"center="+latitude+","+longitude+"&zoom="+zoom_level
-    # Now we add the map parameters:size, scale (default is scale=1, for high resolution use scale=2),format (.png is default),maptype (select among roadmap,satellite,hybrid, and terrain)
-    API=API+"&size="+length_level+"x"+height_level+"&scale="+scale_level+"&format="+format_type+"&maptype="+maptype
-    # Finally, the key is added:
-    API=API+"&key="+key
-    print(API)
+    url = "https://maps.googleapis.com/maps/api/staticmap"
+    d = {
+        "center": "{},{}".format(latitude,longitude),
+        "zoom": zoom_level,
+        "size": "{}x{}".format(length_level,height_level),
+        "scale": scale_level,
+        "format": format_type,
+        "maptype": maptype,
+        "key": key
+    }
+    response = requests.get(url, params=d)
+    print(response.url)    
 
 latitude="35.3505823"
 longitude="-117.8108475"
 zoom_level="10"
 # The relationship between the zoom and the map scale is given by https://stackoverflow.com/questions/9356724/google-map-api-zoom-range 
-meter_per_pixel=156543.03392 * math.cos(float(latitude)* math.pi / 180) / math.pow(2, float(zoom_level))
+meter_per_pixel=156543.03392 * math.cos(math.radians(float(latitude))) / math.pow(2, float(zoom_level))
+inv_mpp = math.log(math.cos(math.radians(float(latitude)))/meter_per_pixel*156543.03392,2) # Invert meter_per_pixel to zoom_level
 length_level="400"
 height_level="400"
 scale_level="2"
