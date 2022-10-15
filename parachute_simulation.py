23bd8250abbe1ebcd5ae2deeeeecbf4189addca6
Allen Chang <allen.chang@gatech.edu>
2022-10-15 14:56:16 -0400

    Updated parachute dispersion analysis script for two-object drop
    Updated parachute dispersion analysis script for two-object drop

--- parachute_simulation.py
+++ parachute_simulation.py
@@ -123,24 +123,39 @@ mpl.rcParams['legend.fontsize'] = 14
 mpl.rcParams['figure.titlesize'] = 14
 
 # Time for weather data, adjust in 6 hour increments
-DATE = (2022, 9, 30, 12)
+DATE = (2022, 11, 20, 12)
 DIAM = 10
 
-number_of_simulations = 20
+number_of_simulations = 10
 
 # Toggle True/False to deploy parachutes or land ballistic
 parachutes = True
 
 # Rocket data that will vary between each iteration, in the form of (data, uncertainty),
 # varies along a normal distribution
+
 analysis_parameters = {
+    "thrustScaleB": (1, 0.05),
+    "thrustScaleS": (1, 0.05),
+    "ignitionDelay": (x0["ignition_delay"], 3),
+    "throatRadiusB": (np.sqrt(x0["A_t_1"]/np.pi)*0.0254, 0),
+    "throatRadiusS": (np.sqrt(x0["A_t_2"]/np.pi)*0.0254, 0),
     "massNR": (event6["m"], 0),
     "inertiaINR": (event6["I_I"], 0),
     "inertiaZNR": (0.5*event6["m"]*(x0["diam"]/2*0.0254)**2, 0),
+    "finSpan1": (x0["span_1"], 0), #inches
+    "finRoot1": (x0["root_1"], 0), #inches
+    "finTip1": (x0["tip_1"], 0), #inches
+    "finSpan2": (x0["span_2"], 0), #inches
+    "finRoot2": (x0["root_2"], 0), #inches
+    "finTip2": (x0["tip_2"], 0), #inches
+    "boosterParachute": (x0["diachute_1"], 0),
+    "sustainerParachute": (x0["diachute_2"], 0),
     "noseConeParachute": (x0["diachute_3"], 0),
     "heading": (0, 5),
     "inclination": (90, 1),
     "ensembleMember": list(range(10)),
+    "chuteFailure": list(range(100)),
 }
 
 # Helper Function to export data from the flights to a file to be accessed for analysis later
@@ -177,6 +192,8 @@ def flight_settings(analysis_parameters, total_number):
 
 # Creates and opens files to export the flight data to
 nose_output_file = open('nose_analysis_outputs'+'.disp_outputs.txt', 'w')
+obj1_output_file = open('obj1_analysis_outputs'+'.disp_outputs.txt', 'w')
+obj2_output_file = open('obj2_analysis_outputs'+'.disp_outputs.txt', 'w')
 
 # Creates an environment object using launch location data
 Env = Environment(
@@ -184,7 +201,7 @@ Env = Environment(
     latitude=33.9127,
     longitude=-84.9417,
     elevation=1300,
-    date=(2022,10,1,12) # change this <- this is the date of the launch
+    date=(2022,10,20,12) # change this <- this is the date of the launch
     )
 #Env.allInfo()
 # Sets the atmospheric model to an Ensemble type so a new forecast can be used for each iteration
@@ -195,7 +212,7 @@ Env.setAtmosphericModel(type='Ensemble', file='GEFS')
 
 # Parachute triggers
 def drogueTrigger(p, y):
-    return True if y[5] < 0 else False
+    return True if y[2] < 31100 else False
 
 
 def mainTrigger(p, y):
@@ -204,10 +221,7 @@ def mainTrigger(p, y):
 
 # Toggles the parachute cd depending on if the simulation is using parachutes, sets cd to zero to make parachutes have
 # no effect
-cd = 0
-if parachutes:
-    cd = 0.97
-
+cd = 0.97
 # Counts the number of simulations that have run
 i = 0
 
@@ -245,41 +259,109 @@ for setting in flight_settings(analysis_parameters, number_of_simulations):
 
     Event6.setRailButtons([0.1, -0.2])
 
-    Event6Main = Event6.addParachute('Main',
-                                    CdS=cd*np.pi*(0.3048*setting['noseConeParachute']/2)**2,
-                                    trigger=mainTrigger,
-                                    samplingRate=105,
-                                    lag=1.5,
-                                    noise=(0, 8.3, 0.5))
-    initial_v = 4000 # important !! ✈️ <- change this
+    # Event6Main = Event6.addParachute('Main',
+    #                                 CdS=cd*np.pi*(0.3048*setting['noseConeParachute']/2)**2,
+    #                                 trigger=mainTrigger,
+    #                                 samplingRate=105,
+    #                                 lag=1.5,
+    #                                 noise=(0, 8.3, 0.5))
+    initial_v = 60 # important !! ✈️ <- change this
     initial_bearing = 128 # <- and this
     drop_elevation = 30000 # <- and this
+
+    obj1_mass = 10 # <- and this
+    obj2_mass = 10 # <- and this
+    c_d = 0.97 # <- and this
 
     x_velocity = initial_v*np.sin(np.radians(initial_bearing))
     y_velocity = initial_v*np.cos(np.radians(initial_bearing))
 
     initial = [0,0,0,Env.elevation+drop_elevation,x_velocity,y_velocity,0,0,0,0,0,0,0,0.0]
-    #initial = None
 
-    noseRecovery = Flight(rocket=Event6, environment=Env, initialSolution=initial, timeOvershoot = True, maxTime=4000)
-    noseRecovery.postProcess()
+    initial_drop = Flight(rocket=Event6, environment=Env, initialSolution=initial, timeOvershoot = True, maxTime=6)
+    # print(initial_drop.solution)
+    # breakpoint()
+    initial_drop.postProcess()  
 
-    df_nose = pd.DataFrame([[noseRecovery.apogee, noseRecovery.apogeeX, noseRecovery.apogeeY, noseRecovery.xImpact, noseRecovery.yImpact, noseRecovery.impactVelocity]], columns = ['apogeeAltitude', 'apogeeX', 'apogeeY', 'impactX', 'impactY', 'impactVelocity'])
+    after_drop = initial_drop.solution[-1] #[x for x in initial_drop.solution if x[6] < 0][-1]
+    after_drop = [0.0, *after_drop[1:]] #ඞ
+
+
+    Object1 = Rocket( 
+        motor=motor_empty,
+        radius=x0["diam"]/2*0.0254,
+        mass=obj1_mass,
+        inertiaI=setting['inertiaINR'],
+        inertiaZ=setting['inertiaZNR'],
+        distanceRocketNozzle=event6["d_nozzle"],
+        distanceRocketPropellant=event6["d_prop"],
+        powerOffDrag="./CDs/s_power_off.csv",
+        powerOnDrag="./CDs/s_power_on.csv"
+    )
+
+    Object2 = Rocket( 
+        motor=motor_empty,
+        radius=x0["diam"]/2*0.0254,
+        mass=obj2_mass,
+        inertiaI=setting['inertiaINR'],
+        inertiaZ=setting['inertiaZNR'],
+        distanceRocketNozzle=event6["d_nozzle"],
+        distanceRocketPropellant=event6["d_prop"],
+        powerOffDrag="./CDs/s_power_off.csv",
+        powerOnDrag="./CDs/s_power_on.csv"
+    )
+
+    Object1.setRailButtons([0.1, -0.2])
+    Object2.setRailButtons([0.1, -0.2])
+
+    Main1 = Object1.addParachute('Main1',
+        CdS=cd*np.pi*(0.3048*setting['noseConeParachute']/2)**2,            
+        trigger=drogueTrigger,
+        samplingRate=105,
+        lag=1.5,
+        noise=(0, 8.3, 0.5))
+
+    Main2 = Object2.addParachute('Main2',
+        CdS=cd*np.pi*(0.3048*setting['noseConeParachute']/2)**2,            
+        trigger=drogueTrigger,
+        samplingRate=105,
+        lag=1.5,
+        noise=(0, 8.3, 0.5))
+
+    obj1_flight = Flight(rocket=Object1, environment=Env, initialSolution=after_drop, timeOvershoot = True, maxTime=4000)
+    obj1_flight.postProcess()
+
+    obj2_flight = Flight(rocket=Object2, environment=Env, initialSolution=after_drop, timeOvershoot = True, maxTime=4000)
+    obj2_flight.postProcess()
+
+    df_settings = pd.DataFrame([setting])
+    df_obj1 = pd.DataFrame([[obj1_flight.apogee, obj1_flight.apogeeX, obj1_flight.apogeeY, obj1_flight.xImpact, obj1_flight.yImpact, obj1_flight.impactVelocity]], columns = ['apogeeAltitude', 'apogeeX', 'apogeeY', 'impactX', 'impactY', 'impactVelocity'])
+    df_obj2 = pd.DataFrame([[obj2_flight.apogee, obj2_flight.apogeeX, obj2_flight.apogeeY, obj2_flight.xImpact, obj2_flight.yImpact, obj2_flight.impactVelocity]], columns = ['apogeeAltitude', 'apogeeX', 'apogeeY', 'impactX', 'impactY', 'impactVelocity'])
+    df_nose = pd.DataFrame([[initial_drop.apogee, initial_drop.apogeeX, initial_drop.apogeeY, initial_drop.xImpact, initial_drop.yImpact, initial_drop.impactVelocity]], columns = ['apogeeAltitude', 'apogeeX', 'apogeeY', 'impactX', 'impactY', 'impactVelocity'])
 
     if os.path.exists("df_nose.csv"):
         if os.stat("df_nose.csv").st_size <= 4:
+            df_obj1.to_csv("df_obj1", mode='a', header=True, index=False)
+            df_obj2.to_csv("df_obj2", mode='a', header=True, index=False)
             df_nose.to_csv("df_nose.csv", mode = 'a', index = False, header = True)
         else:
+            df_obj1.to_csv("df_obj1", mode='a', header=False, index=False)
+            df_obj2.to_csv("df_obj2", mode='a', header=False, index=False)
             df_nose.to_csv("df_nose.csv", mode = 'a', index = False, header = False)
     else:
+        df_obj1.to_csv("df_obj1", mode='a', header=True, index=False)
+        df_obj2.to_csv("df_obj2", mode='a', header=True, index=False)
         df_nose.to_csv("df_nose.csv", mode = 'a', index = False, header = True)
 
-
-    export_flight_data(noseRecovery, nose_output_file)
+    export_flight_data(obj1_flight, obj1_output_file)
+    export_flight_data(obj2_flight, obj2_output_file)
+    export_flight_data(initial_drop, nose_output_file)
 
     print("Simulations Completed: " + str(i))
 
 # Closes export files
+obj1_output_file.close()
+obj2_output_file.close()
 nose_output_file.close()
 
 # Extracts the data from each export file
@@ -303,6 +385,9 @@ def extractData(filename):
 
 
 nose_results = extractData('nose_analysis_outputs')
+obj1_results = extractData('obj1_analysis_outputs')
+obj2_results = extractData('obj2_analysis_outputs')
+
 
 # Print number of flights simulated
 print('Total number of simulations: ', i)
@@ -312,6 +397,18 @@ print(f'Nose Impact X Position - Standard Deviation: {np.std(nose_results["impac
 
 print(f'Nose Impact Y Position -         Mean Value: {np.mean(nose_results["impactY"]):0.3f} m')
 print(f'Nose Impact Y Position - Standard Deviation: {np.std(nose_results["impactY"]):0.3f} m')
+
+print(f'Object 1 Impact X Position -         Mean Value: {np.mean(obj1_results["impactX"]):0.3f} m')
+print(f'Object 1 Impact X Position - Standard Deviation: {np.std(obj1_results["impactX"]):0.3f} m')
+
+print(f'Object 1 Impact Y Position -         Mean Value: {np.mean(obj1_results["impactY"]):0.3f} m')
+print(f'Object 1 Impact Y Position - Standard Deviation: {np.std(obj1_results["impactY"]):0.3f} m')
+
+print(f'Object 2 Impact X Position -         Mean Value: {np.mean(obj2_results["impactX"]):0.3f} m')
+print(f'Object 2 Impact X Position - Standard Deviation: {np.std(obj2_results["impactX"]):0.3f} m')
+
+print(f'Object 2 Impact Y Position -         Mean Value: {np.mean(obj2_results["impactY"]):0.3f} m')
+print(f'Object 2 Impact Y Position - Standard Deviation: {np.std(obj2_results["impactY"]):0.3f} m')
 
 end = time.time()
 print("Elapsed time: ", end - start)
@@ -324,6 +421,10 @@ ApogeeX = np.array(nose_results['apogeeX'])
 ApogeeY = np.array(nose_results['apogeeY'])
 noseImpactX = np.array(nose_results['impactX'])
 noseImpactY = np.array(nose_results['impactY'])
+obj1ImpactX = np.array(obj1_results['impactX'])
+obj1ImpactY = np.array(obj1_results['impactY'])
+obj2ImpactX = np.array(obj2_results['impactX'])
+obj2ImpactY = np.array(obj2_results['impactY'])
 df_nose = pd.DataFrame.from_dict(nose_results) 
 
 df_nose.to_csv("df_nose.csv", index = False)
@@ -336,46 +437,50 @@ def eigsorted(cov):
 
 
 # Create plot figure
-plt.figure(num=None, figsize=(8, 8), dpi=150, facecolor='w', edgecolor='k')
-ax = plt.subplot(111)
+def plot(x,y, name):
+    plt.figure(num=None, figsize=(8, 8), dpi=150, facecolor='w', edgecolor='k')
+    ax = plt.subplot(111)
 
-# Calculate error ellipses for impact
-impactCov = np.cov(np.negative(noseImpactX), np.negative(noseImpactY))
-impactVals, impactVecs = eigsorted(impactCov)
-impactTheta = np.degrees(np.arctan2(*impactVecs[:,0][::-1]))
-impactW, impactH = 2 * np.sqrt(impactVals)
-plt.scatter(np.negative(noseImpactX), np.negative(noseImpactY), s=5, marker='v', color='blue', label='Nose Cone Landing Point')
-plt.legend()
+    # Calculate error ellipses for impact
+    impactCov = np.cov(np.negative(x), np.negative(y))
+    impactVals, impactVecs = eigsorted(impactCov)
+    impactTheta = np.degrees(np.arctan2(*impactVecs[:,0][::-1]))
+    impactW, impactH = 2 * np.sqrt(impactVals)
+    plt.scatter(np.negative(x), np.negative(y), s=5, marker='v', color='red', label=f'{name}Landing Point')
+    plt.legend()
 
-ax.set_title('1$\sigma$, 2$\sigma$ and 3$\sigma$ Dispersion Ellipses: Landing Points')
-ax.set_ylabel('North (m)')
-ax.set_xlabel('East (m)')
-ax.tick_params(axis='both', which='major', labelsize=10)
-ax.tick_params(axis='both', which='minor', labelsize=8)
+    ax.set_title('1$\sigma$, 2$\sigma$ and 3$\sigma$ Dispersion Ellipses: Landing Points')
+    ax.set_ylabel('North (m)')
+    ax.set_xlabel('East (m)')
+    ax.tick_params(axis='both', which='major', labelsize=10)
+    ax.tick_params(axis='both', which='minor', labelsize=8)
 
-# Add background image to plot, translate map by changing dx and dy
-dx = 0
-dy = 0
-plt.imshow(img, zorder=0, extent=[-35000-dx, 35000-dx, -35000-dy, 35000-dy])
-plt.axhline(0, color='black', linewidth=0.5)
-plt.axvline(0, color='black', linewidth=0.5)
-plt.xlim(-35000, 35000)
-plt.ylim(-35000, 35000)
+    # Add background image to plot, translate map by changing dx and dy
+    dx = 0
+    dy = 0
+    plt.imshow(img, zorder=0, extent=[-35000-dx, 35000-dx, -35000-dy, 35000-dy])
+    plt.axhline(0, color='black', linewidth=0.5)
+    plt.axvline(0, color='black', linewidth=0.5)
+    plt.xlim(-35000, 35000)
+    plt.ylim(-35000, 35000)
 
-# Draw error ellipses for nose cone impact
-impact_ellipses = []
-for j in [1, 2, 3]:
-    impactEll = Ellipse(xy=(np.mean(np.negative(noseImpactX)), np.mean(np.negative(noseImpactY))),
-                        width=impactW*j, height=impactH*j,
-                        angle=impactTheta, color='black')
-    impactEll.set_facecolor((0, 0, 1, 0.2))
-    impact_ellipses.append(impactEll)
-    ax.add_artist(impactEll)
+    # Draw error ellipses for nose cone impact
+    impact_ellipses = []
+    for j in [1, 2, 3]:
+        impactEll = Ellipse(xy=(np.mean(np.negative(x)), np.mean(np.negative(y))),
+                            width=impactW*j, height=impactH*j,
+                            angle=impactTheta, color='black')
+        impactEll.set_facecolor((0, 0, 1, 0.2))
+        impact_ellipses.append(impactEll)
+        ax.add_artist(impactEll)
 
 
-filename = 'Dispersion Analysis Map'
-plt.savefig(str(filename) + '.pdf', bbox_inches='tight', pad_inches=0)
+    filename = f'{name}Dispersion Analysis Map'
+    plt.savefig(str(filename) + '.pdf', bbox_inches='tight', pad_inches=0)
 
-plt.show()
+    plt.show()
+
+plot(obj1ImpactX,obj1ImpactY, 'Object 1 ')
+plot(obj2ImpactX,obj2ImpactY, 'Object 2 ')
 
 print("Elapsed time: ", end - start)
